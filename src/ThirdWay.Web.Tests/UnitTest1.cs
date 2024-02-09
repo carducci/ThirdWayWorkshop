d0ef1a4d22b18164566b1989157d0d3aa652c1a8
carducci <michael@trulymagic.com>
2024-02-09 16:08:59 -0700

    Add project files.

---
+++ src/ThirdWay.Web.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ThirdWay.Web.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SanityCheck()
+        {
+            Assert.Pass();
+        }
+    }
+}
