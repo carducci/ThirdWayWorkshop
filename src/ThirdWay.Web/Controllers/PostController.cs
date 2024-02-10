8664eccb7afea5d90da1a9c4f0ae309c1a682848
carducci <michael@trulymagic.com>
2024-02-10 14:54:30 -0700

    F!! Initial View in place for all posts

---
+++ src/ThirdWay.Web/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThirdWay.Data;
+
+namespace ThirdWay.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ReaderContext _context;
+
+        public PostController(ILogger<HomeController> logger, ReaderContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var posts = _context.Posts.ToList();
+            return View(posts);
+        }
+    }
+}
