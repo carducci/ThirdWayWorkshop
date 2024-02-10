d72b6e1b383a65c5fd132fb94d3ee8f9c7052efa
carducci <michael@trulymagic.com>
2024-02-09 18:08:52 -0700

    e Correcting scaffold

---
+++ src/ThirdWay.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThirdWay.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
